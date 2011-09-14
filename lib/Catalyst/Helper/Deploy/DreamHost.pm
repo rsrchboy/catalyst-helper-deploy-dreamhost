67d40a9733d6d155181ba5319e23bd4d4672bcfc
Chris Weyl <cweyl@alumni.drew.edu>
2011-09-13 21:37:09 -0700

    initial helper code

--- lib/Catalyst/Helper/Deploy/DreamHost.pm
+++ lib/Catalyst/Helper/Deploy/DreamHost.pm
@@ -1,53 +1,96 @@
 package Catalyst::Helper::Deploy::DreamHost;
 
-# ABSTRACT: The great new Catalyst::Helper::Deploy::DreamHost!
+# ABSTRACT: Create a couple files useful when deploying to DreamHost
 
-use Moose;
 use namespace::autoclean;
 use common::sense;
 
+use Path::Class;
 
-__PACKAGE__->meta->make_immutable;
+sub mk_stuff {
+    my ($self, $helper, $args) = @_;
+
+    #my $app = lc $helper->{app};
+
+    my $base = $helper->{base};
+    my $app  = lc $helper->{app};
+
+    $app =~ s/::/_/g;
+
+    my $dir = dir qw{ deploy dreamhost };
+    $helper->mk_dir("$dir");
+
+    #my %files = (
+    #    htaccess => file $dir, 'htaccess';
+    my $script = file $dir, 'htaccess';
+    $helper->render_file($_, file($dir, $_)->stringify)
+        for qw{ htaccess dispatch.fcgi };
+
+    return;
+}
 
 1;
 
-__END__
-
 =head1 SYNOPSIS
 
-In C<dist.ini>:
-
-    [NoSmartCommentsTests]
+./script/myapp_create.pl Deploy::DreamHost
 
 =head1 DESCRIPTION
 
-This is an extension of L<Dist::Zilla::Plugin::InlineFiles>, providing the
-following file:
+This is a Catalyst helper module that builds a basic htaccess and
+dispatch.fcgi that you can use to assist in deploying your Catalyst app to
+DreamHost.
 
-    xt/release/no-smart-comments.t - test to ensure no Smart::Comments
+VERY EARLY CODE: things may yet change, but shouldn't impact older versions
+(unless you rerun the helper, natch).
 
-=head1 NOTE
+=head1 TODO
 
-The name of this plugin has turned out to be somewhat misleading, I'm afraid:
-we don't actually test for the _existance_ of smart comments, rather we
-ensure that Smart::Comment is not used by any file checked.
-
-=head1 SEE ALSO
-
-L<Smart::Comments>, L<Test::NoSmartComments>
-
-=head1 BUGS
-
-All complex software has bugs lurking in it, and this module is no exception.
-
-Please report any bugs to
-"bug-Catalyst::Helper::Deploy::DreamHost@rt.cpan.org",
-or through the web interface at <http://rt.cpan.org>.
-
-Patches and pull requests through GitHub are most welcome; our page and repo
-(same URI):
-
-    https://github.com/RsrchBoy/Catalyst::Helper::Deploy::DreamHost
+Git hook?
 
 =cut
 
+__DATA__
+__htaccess__
+
+Options +ExecCGI +FollowSymLinks
+AddHandler fastcgi-script .fcgi
+
+# ----------------------------------------------------------------------
+# UTF-8 encoding
+# ----------------------------------------------------------------------
+
+# Use UTF-8 encoding for anything served text/plain or text/html
+AddDefaultCharset utf-8
+
+# Force UTF-8 for a number of file formats
+AddCharset utf-8 .html .css .js .xml .json .rss .atom
+
+RewriteEngine On
+RewriteBase /
+
+#RewriteCond %{REQUEST_URI} ^/static.*$ [NC]
+#RewriteRule . - [L]
+
+RewriteCond %{REQUEST_URI} ^/(stats|failed_auth\.html).*$ [NC]
+RewriteRule . - [L]
+RewriteRule ^(dispatch\.fcgi/.*)$ - [L]
+RewriteRule ^(.*)$ dispatch.fcgi/$1 [PT,L]
+
+__dispatch.fcgi__
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use lib "$ENV{HOME}/[% app %]/lib";
+
+use Plack::Handler::FCGI;
+use Plack::Builder;
+
+my $app = builder {
+
+    do "$ENV{HOME}/[% app %]/[% app | lower %].psgi";
+};
+
+Plack::Handler::FCGI->new->run($app);
